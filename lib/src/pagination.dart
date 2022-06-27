adc3987029f4fa3a02a1de43f97981986af2d506
iamngoni <imngonii@gmail.com>
2022-06-27 12:19:49 +0200

    created the necessary pageviews

--- lib/src/pagination.dart
+++ lib/src/pagination.dart
@@ -7,24 +7,41 @@ class ModestPagination<T> extends StatefulWidget {
     required this.items,
     this.innerPaginationCount = 8,
     this.outerPaginationCount = 10,
+    this.innerIconsColor = const Color(0xFF00595F),
+    this.outerIconsColor = const Color(0xFF00595F),
+    this.innerIconsSize = 24,
+    this.outerIconsSize = 24,
+    this.activeIndexColor = const Color(0xFF00595F),
+    this.inactiveIndexColor = const Color(0xFF00595F),
+    this.activeTextSize = 16,
+    this.inactiveTextSize = 16,
   }) : super(key: key);
 
   final List<T> items;
   final int innerPaginationCount;
   final int outerPaginationCount;
+  final Color innerIconsColor;
+  final Color outerIconsColor;
+  final double innerIconsSize;
+  final double outerIconsSize;
+  final Color activeIndexColor;
+  final Color inactiveIndexColor;
+  final double activeTextSize;
+  final double inactiveTextSize;
 
   @override
   State<ModestPagination<T>> createState() => _ModestPaginationState<T>();
 }
 
 class _ModestPaginationState<T> extends State<ModestPagination<T>> {
-
   final PageController innerPageController = PageController(initialPage: 0);
   final PageController outerPageController = PageController(initialPage: 0);
 
+  int index = 0;
+
   List<List<T>>? innerItems;
   List<List<List<T>>>? outerItems;
-
+  List<int>? outerIndexes;
 
   @override
   void initState() {
@@ -32,11 +49,153 @@ class _ModestPaginationState<T> extends State<ModestPagination<T>> {
         as List<List<T>>;
     outerItems = innerItems?.partition(chunkSize: widget.outerPaginationCount)
         as List<List<List<T>>>;
+    outerIndexes = Iterable<int>.generate(outerItems!.length).toList();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return PageView(
+      controller: outerPageController,
+      children: outerIndexes!.map((int outerIndex) {
+        List<List<T>> currentItems = outerItems![outerIndex];
+        int totalPages = currentItems.length;
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: context.width,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(
+                vertical: 10,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      outerPageController.previousPage(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeInOut,
+                      );
+                      innerPageController.jumpToPage(0);
+                      setState(() {
+                        index = 0;
+                      });
+                    },
+                    child: Icon(
+                      Icons.fast_rewind_rounded,
+                      color: widget.outerIconsColor,
+                      size: widget.outerIconsSize,
+                    ),
+                  ),
+                  AnimatedCrossFade(
+                    firstChild: InkWell(
+                      onTap: () {
+                        setState(() {
+                          index != 0 ? index-- : 0;
+                        });
+                        innerPageController.jumpToPage(index);
+                      },
+                      child: RotatedBox(
+                        quarterTurns: 2,
+                        child: Icon(
+                          Icons.play_arrow,
+                          color: widget.innerIconsColor,
+                          size: widget.innerIconsSize,
+                        ),
+                      ),
+                    ),
+                    secondChild: const SizedBox.shrink(),
+                    crossFadeState: index != 0
+                        ? CrossFadeState.showFirst
+                        : CrossFadeState.showSecond,
+                    duration: const Duration(milliseconds: 300),
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: List.generate(
+                        totalPages,
+                        (pageIndex) => InkWell(
+                          onTap: () {
+                            setState(() {
+                              index = pageIndex;
+                            });
+                            innerPageController.jumpToPage(index);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 10,
+                            ),
+                            child: Text(
+                              "${(outerIndex * widget.outerPaginationCount) + (pageIndex + 1)}",
+                              style: TextStyle(
+                                color: index == pageIndex
+                                    ? widget.activeIndexColor
+                                    : widget.inactiveIndexColor,
+                                fontWeight: index == pageIndex
+                                    ? FontWeight.bold
+                                    : FontWeight.normal,
+                                fontSize: index == pageIndex
+                                    ? widget.activeTextSize
+                                    : widget.inactiveTextSize,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  AnimatedCrossFade(
+                    firstChild: InkWell(
+                      onTap: () {
+                        setState(() {
+                          index != totalPages - 1 ? index++ : index = 0;
+                        });
+                        innerPageController.jumpToPage(index);
+                      },
+                      child: Icon(
+                        Icons.play_arrow,
+                        color: widget.innerIconsColor,
+                        size: widget.innerIconsSize,
+                      ),
+                    ),
+                    secondChild: const SizedBox.shrink(),
+                    crossFadeState: index != totalPages - 1
+                        ? CrossFadeState.showFirst
+                        : CrossFadeState.showSecond,
+                    duration: const Duration(milliseconds: 300),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      outerPageController.nextPage(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeInOut,
+                      );
+                      innerPageController.jumpToPage(0);
+                      setState(() {
+                        index = 0;
+                      });
+                    },
+                    child: Icon(
+                      Icons.fast_forward_rounded,
+                      color: widget.outerIconsColor,
+                      size: widget.outerIconsSize,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        );
+      }).toList(),
+    );
   }
 }
